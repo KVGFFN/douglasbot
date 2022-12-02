40dd2baaf76829f2faa36cf1bd6bcd3b9ed60f5e
KVGFFN <kaine.vangeffen@gmail.com>
2022-12-02 23:15:37 +0100

    edit main

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ html_content = requests.get(url).text
 soup = BeautifulSoup(html_content, "html.parser")
 
 # Helper.getProductByFormat("https://www.douglas.be/nl/p/3001047702", "100 ml")
+print("test build")
