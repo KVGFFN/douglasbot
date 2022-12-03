73501f43ed6a6bb910f5330eb36a78fd45c02362
KVGFFN <kaine.vangeffen@gmail.com>
2022-12-03 18:27:42 +0100

    edit: helper message

--- helper.py
+++ helper.py
@@ -4,6 +4,7 @@ import requests
 class Helper():
 
     def getProductByFormat(url, format):
+        print("-- checking product status --")
         # variables
         soup = BeautifulSoup(requests.get(url).text, "html.parser")
         product_detail__variant_names = soup.find_all("div", {"class": "product-detail__variant-name"})
@@ -11,9 +12,7 @@ class Helper():
 
         for product_detail__variant_name in product_detail__variant_names:
             if (product_detail__variant_name is not None):
-                print(product_detail__variant_name.text)
                 if (product_detail__variant_name.text == format):
-                    print(f'Found {format} in {url}')
                     parent_product_name = product_detail__variant_name.find_parent("div", {"class": "product-detail__variant-row product-detail__variant-row--spread-content"})
 
                     child_product_detail_appfold = parent_product_name.find("div", {"class": "product-detail__variant-row--app-fold"})
@@ -21,10 +20,10 @@ class Helper():
 
                     span_status = child_stockstatus.find("span", {"class": "status_available_in_future"})
                     if (span_status is not None):
-                        print("NOT AVAILABLE")
-                        print(span_status.text)
+                        print(f"{format} isn't available yet :cry:" )
                         break
                     else:
-                        print("Is available")
+                        print(f":warning: {format} AVAILABLE :warning: @everyone")
+                        print(url)
                         break
                     
